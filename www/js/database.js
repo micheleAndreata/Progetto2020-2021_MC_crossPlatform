0bfe78283f1d276a0fa9fa66ee17c01eef0e9446
micheleAndreata <michele.andreata@studenti.unimi.it>
2021-01-08 11:28:00 +0100

    fix database

--- www/js/database.js
+++ www/js/database.js
@@ -1,75 +1,49 @@
-var db = null;
 
 function setTableDatabase() {
-    db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' }, function (db) {
-    
-        db.transaction(function(tx) {
-
-            tx.executeSql('CREATE TABLE IF NOT EXISTS postImage (pid STRING PRIMARY KEY, picture STRING)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS userPicture (uid STRING PRIMARY KEY, picture STRING, pversion INTEGER)');
-    
-            
-        }, function(error) {
-            console.log('transaction setTableDatabase error: ' + error.message);
-            db.close();
-            console.log('database is closed ok');
-        }, function() {
-            console.log('transaction setTableDatabase ok');
-            db.close(function() {
-                console.log('database is closed ok');
-            });
+    let db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' }, (db) => {
+        db.transaction(
+                (tx) => {
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS postImage (pid STRING PRIMARY KEY, picture STRING)');
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS userPicture (uid STRING PRIMARY KEY, picture STRING, pversion INTEGER)');
+                }, 
+                (tx, error) => db.close(() => console.error("setTable", "transaction error: " + JSON.stringify(error))),
+                () => db.close(() => console.log("setTable", 'transaction ok. Database is closed')));
+        }, (error) => {
+            console.error('Open database ERROR: ' + JSON.stringify(error));
         });
-    
-    }, function (error) {
-        console.log('Open database ERROR: ' + JSON.stringify(error));
-    });
 }
 
-// function setTableDatabase() {
-//     db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' }, (db) => {
-//         db.transaction(
-//                 (tx) => {
-//                     tx.executeSql('CREATE TABLE IF NOT EXISTS postImage (pid STRING PRIMARY KEY, picture STRING)');
-//                     tx.executeSql('CREATE TABLE IF NOT EXISTS userPicture (uid STRING PRIMARY KEY, picture STRING, pversion INTEGER)');
-//                 }, 
-//                 (tx, error) => db.close(() => console.error("setTable", "transaction error: " + JSON.stringify(error))),
-//                 () => db.close(() => console.log("setTable", 'transaction ok. Database is closed')));
-//         }, (error) => {
-//             console.error('Open database ERROR: ' + JSON.stringify(error));
-//         });
-// }
-
-function insertPostImage(pid, image, callback) {
+function insertPostImage(pid, image, successCallback, errorCallback) {
     let query = "INSERT INTO postImage (pid, picture) VALUES (?,?)";
-    accessDatabase(query, [pid, image], callback, "inserPostImage");
+    accessDatabase(query, [pid, image], successCallback, errorCallback, "inserPostImage");
 }
 
-function insertUserPicture(uid, picture, pversion, callback) {
+function insertUserPicture(uid, picture, pversion, successCallback, errorCallback) {
     let query = "INSERT INTO userPicture (uid, picture, pversion) VALUES (?,?,?)";
-    accessDatabase(query, [uid, picture, pversion], callback, "inserUserPicture");
+    accessDatabase(query, [uid, picture, pversion], successCallback, errorCallback, "inserUserPicture");
 }
 
-function updateUserPicture(uid, picture, pversion, callback) {
+function updateUserPicture(uid, picture, pversion, successCallback, errorCallback) {
     let query = "UPDATE userPicture SET picture = ? pversion = ? WHERE uid = ?";
-    accessDatabase(query, [picture, pversion, uid], callback, "updateUserPicture");
+    accessDatabase(query, [picture, pversion, uid], successCallback, errorCallback, "updateUserPicture");
 }
 
 
-function getPostImages(callback) {
+function getPostImages(successCallback, errorCallback) {
     let query = "SELECT * FROM postImage";
-    accessDatabase(query, [], callback, "getPostImages");
+    accessDatabase(query, [], successCallback, errorCallback, "getPostImages");
 }
 
-function getUserPictures(callback) {
+function getUserPictures(successCallback, errorCallback) {
     let query = "SELECT * FROM userPicture";
-    accessDatabase(query, [], callback, "getUserPictures");
+    accessDatabase(query, [], successCallback, errorCallback, "getUserPictures");
 }
 
-function accessDatabase(query, inputs, onExecuteSuccess, execution_tag){
-    db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' }, 
+function accessDatabase(query, inputs, onExecuteSuccess, onExecuteError, execution_tag){
+    let db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' }, 
         (db) => {
             db.transaction(
-                (tx) => tx.executeSql(query, inputs, onExecuteSuccess), 
+                (tx) => tx.executeSql(query, inputs, onExecuteSuccess, onExecuteError), 
                 (tx, error) => db.close(() => console.error(execution_tag, "Database is closed. Transaction error: " + JSON.stringify(error))),
                 () => db.close(() => console.log(execution_tag, 'transaction ok. Database is closed')));
         }, (error) => {
